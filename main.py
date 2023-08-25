c1978e389762bf32a483a87663be6bbaeda5db01
pedrolorandi <pedrokl@hotmail.com>
2023-08-25 10:36:21 -0400

    Add .env file

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from pygooglenews import GoogleNews
 import newspaper
 import requests
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# API keys
+openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 gn = GoogleNews()
-term = gn.search('Elon Musk twitter x', when = "24h")
-entries = term['entries']
+search = gn.search('twitter', when = "24h")
+entries = search['entries']
 
-
-for entry in entries:
+for entry in entries[:10]:
   print("Title:", entry['title'])
   link = entry['links'][0]['href']
   response = requests.get(link)
 
-  if response.status_code == 200:
-    article = newspaper.Article(link)
-    article.download()
-    article.parse()
-    text = article.text
-    print(text)
-  else:
-    print(f"Failed to retrieve the page for {entry['title']}")
+  # if response.status_code == 200:
+  #   article = newspaper.Article(link)
+  #   article.download()
+  #   article.parse()
+  #   text = article.text
+  #   print(text)
+  # else:
+  #   print(f"Failed to retrieve the page for {entry['title']}")
   print("-----------------------------------------------")
   
