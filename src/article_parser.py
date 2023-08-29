827ac52fca6f9c48cd6d96d88ec93997704d02d7
pedrolorandi <pedrokl@hotmail.com>
2023-08-28 20:45:23 -0400

    Fix request error

--- src/article_parser.py
+++ src/article_parser.py
@@ -4,11 +4,12 @@ import re
 
 def parse_article_from_link(link):
     response = requests.get(link)
+    
     if response.status_code != 200:
         return None
     
     article = newspaper.Article(link)
-    article.download()
+    article.set_html(response.content)
     article.parse()
     
     text = article.text
