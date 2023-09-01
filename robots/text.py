f6b91bffd2ee8486fb40a02ca3e8f84e9d9ede56
pedrolorandi <pedrokl@hotmail.com>
2023-09-01 18:31:06 -0400

    Connect OpenAI API

--- robots/text.py
+++ robots/text.py
@@ -83,14 +83,30 @@ def robot(content):
     print("Content generation complete.\n---")
 
   def generate_script(content):
+    print("Generating script")
+
     # Load the .env file
     load_dotenv()
 
     # API keys
     openai.organization = os.environ.get('OPENAI_ORG_ID')
     openai.api_key = os.environ.get('OPENAI_API_KEY')
+    
+    user_prompt = content['content'] + "With the information provided, write a youtube script using informal, opinionated, and somewhat satirical tone. Use casual language and often employ sarcasm to make points. Use a sense of skepticism towards media narratives and a desire to present a more down-to-earth perspective on events. The tone should be somewhat confrontational at times, challenging conventional ideas and encouraging the audience to think critically. Overall, the tone should be a blend of commentary, critique, and humor. To add pauses in the script, add a simple dash (-) or the em-dash (—). The script should contain 2000 words. Don't write introduction and conclusion paragraphs. Don't welcome back people to the channel, and don't use conclusive phrases like 'See you next time'. WRITE THE SPEAKING PART ONLY."
+
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        temperature=1,
+        messages=[
+          {"role": "user", "content": user_prompt}
+        ]
+    )
+
+    content['script'] = response.choices[0].message.content
+
+    print("Script generation complete.\n---")
 
   fetch_news(content['searchTerm'])
   parse_news(content)
   generate_content(content)
-  # generate_script(content)
+  generate_script(content)
