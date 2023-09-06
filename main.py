bc24c0d9107f684fbb9b944983203bbbf15e18e9
pedrolorandi <pedrokl@hotmail.com>
2023-09-05 21:47:43 -0400

    Remove comments

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# from src import create_content_from_articles, create_script_from_content, create_audio_from_phrases
-
-# MAX_VALID_ARTICLES = 5
-
-# content = create_content_from_articles('twitter', MAX_VALID_ARTICLES)
-# phrases = create_script_from_content(content)
-# audios = create_audio_from_phrases(phrases)
-
 from robots import term_robot, text_robot, StateHandler
 
 robots = {
